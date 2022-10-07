d36c1f00f8e265a3707645286e543b4128648ec6
willor777 <wcorwig777@gmail.com>
2022-10-07 14:34:40 -0400

    Deployment Setup

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -6,11 +6,21 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main(args: Array<String>): Unit {
-    embeddedServer(Netty, port = System.getenv("PORT").toInt()){
 
-    }.start(wait = true)
+    // Check if port is provided (is deployed)
+    val port = System.getenv("PORT")
+
+    // Testing on local machine
+    if (port == null){
+        io.ktor.server.netty.EngineMain.main(args)
+    }
+
+    // Deployed
+    else{
+        embeddedServer(Netty, port = port.toInt()){
+        }.start(wait = true)
+    }
 }
-
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
